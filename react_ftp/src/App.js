89fc1f79f19db52879f0ca6e5bbfbb6262d61f12
yskim <ys.kim@genergyplm.com>
2021-04-22 16:39:06 +0900

    회원가입 페이지 추가

--- react_ftp/src/App.js
+++ react_ftp/src/App.js
@@ -1,14 +1,14 @@
 // import Login from "./Login";
-import Home from "./Home";
-import Test from "./Test";
+// import Home from "./Home";
+import SignUp from "./SignUp.js";
 import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       {/* <Route exact path="/" component={Login} /> */}
-      <Route path="/home" component={Home} onLoad={ Home.HomedataLoad }/>
-      <Route path="/test" component={Test}/>
+      {/* <Route path="/home" component={Home} onLoad={ Home.HomedataLoad }/> */}
+      <Route path="/testYS" component={SignUp}/>
     </Router>
   );
 }
